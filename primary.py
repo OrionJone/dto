eb12f130118b86fd4f7bf9009879824503b5272e
Orion Jones <buddyjones650@gmail.com>
2021-07-01 13:35:12 -0400

    hi everyone updateee

--- primary.py
+++ primary.py
@@ -12,6 +12,12 @@
 #       2 - create a print statement that prints the sum of three numbers
 #       3 - create a print statement the prints the sum of two negative numbers
 
+print(1+1)
+print(1+1+1)
+print(-1+-1)
+
+print(1-1)
+
 
 
 # subtraction
@@ -19,6 +25,11 @@
 #       1 - create a print statement that prints the difference of two numbers
 #       2 - create a print statement that prints the difference of three numbers
 #       3 - create a print statement the prints the difference of two negative numbers
+
+print(1-1)
+print(1-1-1)
+print(-1--1)
+
 
 
 
@@ -28,11 +39,17 @@
 #       2 - create a print statement that prints the dividend of two numbers
 #       3 - create a print statement that evaluates an operation using both multiplication and division
 
+print(1*1)
+print(1/1)
+print(1*1/1)
+
 
 
 # floor division
 #   instructions
 #       1 - using floor division, print the dividend of two numbers.
+
+print(42/3)
 
 
 
@@ -40,11 +57,13 @@
 #   instructions
 #       1 - using exponentiation, print the power of two numbers
 
-
+print(1*1)
 
 # modulus
 #   instructions
 #       1 - using modulus, print the remainder of two numbers
+
+print(56/5)
 
 
 # --------------- Section 2 --------------- #
@@ -56,14 +75,32 @@
 #       2 - print the concatenation of five animals you like
 #       3 - print the concatenation of each word in a phrase
 
+print('Orion Jones')
+print('dogs','cats','raccoons','birds','seal')
+
 
 
 # duplication
 #   instructions
-#       1 - print the duplpication of your first 5 times
+#       1 - print the duplication of your first 5 times
 #       2 - print the duplication of a song you like 10 times
+
+print('goosebumps')
+print('goosebumps')
+print('goosebumps')
+print('goosebumps')
+print('goosebumps')
+print('goosebumps')
+print('goosebumps')
+print('goosebumps')
+print('goosebumps')
+print('goosebumps')
+
+
 
 
 # concatenation and duplpication
 #   instructions
 #       1 - print the concatenation of two strings duplicated 3 times each
+print(dogs % cats))
+print(dogs % cats))
